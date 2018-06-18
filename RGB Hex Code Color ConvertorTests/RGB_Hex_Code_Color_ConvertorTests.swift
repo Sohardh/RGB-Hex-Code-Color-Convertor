4611cc2ca87d941cd58982afc428075f49c3a551
Sohardh <31403178+Sohardh@users.noreply.github.com>
2018-06-18 14:12:55 +0530

    Initial Commit

---
+++ RGB Hex Code Color ConvertorTests/RGB_Hex_Code_Color_ConvertorTests.swift
@@ -0,0 +1,36 @@
+//
+//  RGB_Hex_Code_Color_ConvertorTests.swift
+//  RGB Hex Code Color ConvertorTests
+//
+//  Created by Sohardh Chobera on 18/06/18.
+//  Copyright © 2018 Sohardh Chobera. All rights reserved.
+//
+
+import XCTest
+@testable import RGB_Hex_Code_Color_Convertor
+
+class RGB_Hex_Code_Color_ConvertorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
